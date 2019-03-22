b1211eeb210684b4c792f6c90582cf7ad2115152
Amr Abouelkhair <abouelkhair.305@gmail.com>
2019-03-22 15:03:29 -0400

    added nh cylinder to scene lua

--- scene_lua.cpp
+++ scene_lua.cpp
@@ -244,6 +244,32 @@ int gr_nh_box_cmd(lua_State* L)
   return 1;
 }
 
+// Create a non-hierarchical Cylinder node
+extern "C"
+int gr_nh_cylinder_cmd(lua_State* L)
+{
+  GRLUA_DEBUG_CALL;
+
+  gr_node_ud* data = (gr_node_ud*)lua_newuserdata(L, sizeof(gr_node_ud));
+  data->node = 0;
+
+  const char* name = luaL_checkstring(L, 1);
+
+  glm::vec3 pos;
+  get_tuple(L, 2, &pos[0], 3);
+
+  double xradius = luaL_checknumber(L, 3);
+  double yradius = luaL_checknumber(L, 3);
+  double height = luaL_checknumber(L, 3);
+
+  data->node = new GeometryNode(name, new NonhierCylinder(pos, xradius, yradius, height));
+
+  luaL_getmetatable(L, "gr.node");
+  lua_setmetatable(L, -2);
+
+  return 1;
+}
+
 // Create a polygonal Mesh node
 extern "C"
 int gr_mesh_cmd(lua_State* L)
@@ -525,6 +551,7 @@ static const luaL_Reg grlib_functions[] = {
   {"cube", gr_cube_cmd},
   {"nh_sphere", gr_nh_sphere_cmd},
   {"nh_box", gr_nh_box_cmd},
+  {"nh_cylinder", gr_nh_cylinder_cmd},
   {"mesh", gr_mesh_cmd},
   {"light", gr_light_cmd},
   {"render", gr_render_cmd},
