2b0e064071af241d0673cd69c27a342f7dada056
Amr Abouelkhair <abouelkhair.305@gmail.com>
2019-03-26 21:57:40 -0400

    keep the colour of the object even through reflaction

--- A4.cpp
+++ A4.cpp
@@ -221,9 +221,9 @@ bool ray_color(
             );
 
 
-            col[0] = std::max(0.0f, std::min(1.0f, ks[0] * trans_color[0]));
-            col[1] = std::max(0.0f, std::min(1.0f, ks[1] * trans_color[1]));
-            col[2] = std::max(0.0f, std::min(1.0f, ks[2] * trans_color[2]));
+            col[0] += std::max(0.0f, std::min(1.0f, ks[0] * trans_color[0]));
+            col[1] += std::max(0.0f, std::min(1.0f, ks[1] * trans_color[1]));
+            col[2] += std::max(0.0f, std::min(1.0f, ks[2] * trans_color[2]));
 
           }
         }
